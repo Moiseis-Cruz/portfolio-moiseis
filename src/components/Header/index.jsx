a760cfe65b8a130ddb0268f7e6078716d2b987b2
Moiseis Cruz <moizzito@gmail.com>
2024-05-22 19:55:33 -0300

    I created the initial code for the Header function.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return(
+        <header>
+            <ul>
+                <li><a href="">Início</a></li>
+                <li><a href="">Sobre Mim</a></li>
+                <li><a href="">Habilidades</a></li>
+                <li><a href="">Projetos</a></li>
+            </ul>
+        </header>
+    )
+};
