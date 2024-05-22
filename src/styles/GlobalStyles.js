0f98904f06d4d231edfdf2d0b9a987b9d9f69603
Moiseis Cruz <moizzito@gmail.com>
2024-05-22 20:40:32 -0300

    I added :root styling to GlobalStyles.

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,10 +1,34 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+
+        color-scheme: light dark;
+        color: rgba(255, 255, 255, 0.87);
+        background-color: #242424;
+
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+
+    body {
+        display: flex;
+        /* place-items: center; */
+        background-color: #abacf0;
+        width: 100%;
+        max-width: 1440px;
+        min-width: 320px;
+        min-height: 100vh;
+        margin: auto;
     }
 
     ul {
