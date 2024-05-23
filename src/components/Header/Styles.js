0ecd592d993f944e24a3aa9281200704060d09c1
Moiseis Cruz <moizzito@gmail.com>
2024-05-23 19:33:41 -0300

    Continuing styling of the Header components.

--- src/components/Header/Styles.js
+++ src/components/Header/Styles.js
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 
 export const MyHeader = styled.header`
-    background-color: palevioletred;
+    /* background-color: palevioletred; */
     display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
-    max-width: 1440px;   
+    max-width: 1440px;
+    padding: 30px 40px;
+`
+
+export const Title = styled.h1`
+    width: 120px;
+    height: 120px;
+    font-size: 3.5rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: underline;
+    border: 5px solid rgba(255, 255, 255, 0.87);
+`
+
+export const ListSections = styled.ul`
+    background-color: pink;
+    display: flex;
+    gap: 10px;
 `
