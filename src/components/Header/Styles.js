55cdd05d88af347553b5d5a98e6fb546d76f19da
Moiseis Cruz <moizzito@gmail.com>
2024-05-23 19:10:18 -0300

    I imported the MyHeader styling into a styling file.

---
+++ src/components/Header/Styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MyHeader = styled.header`
+    background-color: palevioletred;
+    display: flex;
+    width: 100%;
+    max-width: 1440px;   
+`
