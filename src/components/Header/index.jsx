64710dd1bf0ad9e5fd5da7183de30ab2e90fbf7d
Moiseis Cruz <moizzito@gmail.com>
2024-05-23 18:55:56 -0300

    I removed the styling from the MyHeader component.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,22 @@
+import styled from "styled-components";
+
 export const Header = () => {
     return(
-        <header>
+        <MyHeader>
+            <h1>Logo</h1>
             <ul>
                 <li><a href="">Início</a></li>
                 <li><a href="">Sobre Mim</a></li>
                 <li><a href="">Habilidades</a></li>
                 <li><a href="">Projetos</a></li>
             </ul>
-        </header>
+        </MyHeader>
     )
 };
+
+const MyHeader = styled.header`
+    background-color: palevioletred;
+    display: flex;
+    width: 100%;
+    max-width: 1440px;   
+`
