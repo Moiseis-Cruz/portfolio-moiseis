2c0531f0a33c25a69a29e83d44ff6d54d2cdefee
Moiseis Cruz <moizzito@gmail.com>
2024-05-25 09:41:28 -0300

    I created the <Hero /> file and its function.

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+export const Hero = () => {
+    return(
+        <section>
+            <div>
+                <h1>Título</h1>
+
+                <a href="">Botão</a>
+            </div>
+
+            <img src="" alt="" />
+        </section>
+    )
+}
