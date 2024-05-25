fe393f630d66f61acacaff86347cba164de6cbb0
Moiseis Cruz <moizzito@gmail.com>
2024-05-25 09:40:41 -0300

    I added <Hero /> to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header';
+import { Hero } from './components/Hero';
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
