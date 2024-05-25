1d2d9a56eac00e8dab10aa9f07efda0a680fc2cf
Moiseis Cruz <moizzito@gmail.com>
2024-05-25 09:46:47 -0300

    I styled the <Hero /> Container

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,13 +1,23 @@
+import styled from "styled-components";
+import ImgMyPerfil from "../../assets/imgs/meu-perfil.png"
+
 export const Hero = () => {
     return(
-        <section>
+        <Container>
             <div>
                 <h1>Título</h1>
 
                 <a href="">Botão</a>
             </div>
 
-            <img src="" alt="" />
-        </section>
+            <img src={ImgMyPerfil} alt="" />
+        </Container>
     )
-}
+};
+
+const Container = styled.section`
+    background-color: brown;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`
