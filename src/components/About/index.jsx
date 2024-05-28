608bda25938afd4049e5e3e96884c2df9270ecef
Moiseis Cruz <moizzito@gmail.com>
2024-05-27 21:55:22 -0300

    I added an image to the <About /> function

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,3 +1,5 @@
+import BgMyProfile from "../../assets/imgs/meu-perfil.png"
+
 export const About = () => {
     return(
         <section>
@@ -9,7 +11,7 @@ export const About = () => {
 
                     <p>Essas são as principais ferramentas e habilidades que eu trabalho</p>
 
-                    <img src="" alt="" />
+                    <img src={BgMyProfile} alt="" />
                 </div>
 
                 <div>
@@ -24,12 +26,11 @@ export const About = () => {
 
                     <h3>| Quem sou eu</h3>
 
-                    <p></p>
+                    <p>Me chamo Moiseis Cruz, tenho 33 anos e minha paixão por programação começou na infância. Com três anos de experiência em desenvolvimento, me especializei em Front-end. Destaco-me pela habilidade única de integrar programação de design, contribuindo tanto para o desenvolvimento de códigos eficientes quanto para a criação de interfaces intuitivas e atraentes. Acredito firmemente que a combinação de estética e funcionalidade resulta em experiências oline verdadeiramente memoráveis.</p>
 
                     <h3>| Principal Tecnologia</h3>
 
-                    <p>
-                    </p>
+                    <p>Eu sou apaixonado por JavaScript, minha ferramenta predileta, e busco diariamente novas formas de aprimorar minhas capacidades. Essa linguagem versátil me motiva a criar aplicações dinâmicas e interativas, proporcionando uma experiência envolvente para o usuários. Atualmente, atuo como freelancer, onde abraço a diversidade de projetos e enfrento desafio que constantemente estimulam meu crescimento profissional, incentivando minha busca contínua por aprendizado.</p>
                 </div>
             </div>
         </section>
