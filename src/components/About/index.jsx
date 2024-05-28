12c207f1f5be41fb91dadfc50bd4291a3fa82e71
Moiseis Cruz <moizzito@gmail.com>
2024-05-27 21:35:52 -0300

    Starting the About marking.

---
+++ src/components/About/index.jsx
@@ -0,0 +1,37 @@
+export const About = () => {
+    return(
+        <section>
+            <h2>Sobre.</h2>
+
+            <div>
+                <div>
+                    <h3>Minhas ferramentas e habilidades</h3>
+
+                    <p>Essas são as principais ferramentas e habilidades que eu trabalho</p>
+
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <ul>
+                        <li>React js</li>
+                        <li>JavaScript</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                        <li>Github</li>
+                        <li>Git</li>
+                    </ul>
+
+                    <h3>| Quem sou eu</h3>
+
+                    <p></p>
+
+                    <h3>| Principal Tecnologia</h3>
+
+                    <p>
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+};
