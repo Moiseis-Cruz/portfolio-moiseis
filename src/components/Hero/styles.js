55ac9f5bd8cd958824cebcab9a65a5149bb94a3c
Moiseis Cruz <moizzito@gmail.com>
2024-05-28 22:30:14 -0300

    I added all the styles for <Hero />

---
+++ src/components/Hero/styles.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    min-height: 100vh;
+    @media (max-width: 1024px) {
+        text-align: center;
+        flex-direction: column;
+        gap: 30px;
+        padding: 30px 0;
+    }
+`
+
+export const Title = styled.h1`
+    font-size: 3.5rem;
+`
+
+export const ContentImg = styled.div`
+    max-width: 450px;
+    border-radius: 50%;
+    padding: 15px;
+    box-shadow: inset 0 0 3px 3px rgb(119, 119, 119);
+    /* @media (max-width: 375px) {
+        padding: 0;
+    } */
+    `
+
+export const ImgHero = styled.img`
+    margin: auto;
+    border-radius: 50%;
+    box-shadow: 0 0 3px 3px rgb(119, 119, 119);
+
+    background: linear-gradient(to right, #b688dd, #581051);
+    background-size: 180% 180%;
+    animation: gradient-animation 5s ease infinite;
+
+    @keyframes gradient-animation {
+    0% {
+        background-position: 0% 50%;
+    }
+
+    50% {
+        background-position: 100% 50%;
+    }
+
+    100% {
+        background-position: 0% 50%;
+    }
+}
+`
+
+export const BtnContact = styled.a`
+    margin-top: 30px;
+    display: inline-block;
+    font-size: 18px;
+    padding: 10px 20px;
+    font-weight: 500;
+    background-color: #b688dd;
+    border-radius: 8px;
+`
