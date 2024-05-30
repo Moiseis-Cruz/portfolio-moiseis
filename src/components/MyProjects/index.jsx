08871c1a5fb35e4352b47bb2a7e12410d0a6ec46
Moiseis Cruz <moizzito@gmail.com>
2024-05-30 20:37:41 -0300

    Continuing the marking of <MyProjects />

--- src/components/MyProjects/index.jsx
+++ src/components/MyProjects/index.jsx
@@ -1,7 +1,13 @@
 export const MyProjects = () => {
     return(
         <section>
-            
+            <h2>Meus Projetos</h2>
+
+            <p>Aqui estão os meus principais projetos e mais recentes. Aqui você poderá navegar por eles para conhecer melhor o meu trabalho. 😊</p>
+
+            <div>
+                
+            </div>
         </section>
     )
 }
