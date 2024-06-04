b9492449f48663dc44808cbd2d202dbbaf3eb618
Moiseis Cruz <moizzito@gmail.com>
2024-06-04 12:28:53 -0300

    I removed the elements from the function. I added it to the components.

--- src/components/MyProjects/index.jsx
+++ src/components/MyProjects/index.jsx
@@ -9,9 +9,59 @@ export const MyProjects = () => {
 
             <div>
                 
+                <Card>
+                    <img src="" alt="" />
+                    <h2>Título do Card</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
+
+                    <p>Tecnologias: HTML, CSS, JS</p>
+
+                    <div>
+                        <a href="/">Projeto (live)</a>
+                        <a href="/">Repositório</a>
+                    </div>
+                </Card>
+
+                <Card>
+                    <img src="" alt="" />
+                    <h2>Título do Card</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
+
+                    <p>Tecnologias: HTML, CSS, JS</p>
+
+                    <div>
+                        <a href="/">Projeto (live)</a>
+                        <a href="/">Repositório</a>
+                    </div>
+                </Card>
+
+                <Card>
+                    <img src="" alt="" />
+                    <h2>Título do Card</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
+
+                    <p>Tecnologias: HTML, CSS, JS</p>
+
+                    <div>
+                        <a href="/">Projeto (live)</a>
+                        <a href="/">Repositório</a>
+                    </div>
+                </Card>
+
+                <Card>
+                    <img src="" alt="" />
+                    <h2>Título do Card</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
+
+                    <p>Tecnologias: HTML, CSS, JS</p>
+
+                    <div>
+                        <a href="/">Projeto (live)</a>
+                        <a href="/">Repositório</a>
+                    </div>
+                </Card>
             </div>
 
-            <Card />
         </section>
     )
 };
