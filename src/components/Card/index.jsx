894a3012def8aebec9815e6fbc8583fc850f8df7
Moiseis Cruz <moizzito@gmail.com>
2024-06-04 12:19:42 -0300

    Starting <Card/> styling

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+
 export const Card = () => {
     return(
-        <div>
+        <Container>
             <img src="" alt="" />
             <h2>Título do Card</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
@@ -11,6 +13,18 @@ export const Card = () => {
                 <a href="/">Projeto (live)</a>
                 <a href="/">Repositório</a>
             </div>
-        </div>
+        </Container>
     )
-}
+};
+
+const Container = styled.div`
+    /* background-color: aqua; */
+    background: linear-gradient(0deg, rgba(227, 10, 206,1) 35%, rgba(119, 19, 204,1));
+    max-width: 400px;
+    width: 100%;
+    min-height: 424px;
+    padding: 10px;
+    border-radius: 14px 0;
+    text-align: center;
+    border: 5px solid #581051;
+`
