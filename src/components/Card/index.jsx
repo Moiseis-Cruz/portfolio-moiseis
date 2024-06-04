e2ec159ec34f32c896bb79272cac350b1c316a2d
Moiseis Cruz <moizzito@gmail.com>
2024-06-04 12:28:39 -0300

    I removed the elements from the function. I added it to the components.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,9 @@
 import styled from "styled-components";
 
-export const Card = () => {
+export const Card = ({children}) => {
     return(
         <Container>
-            <img src="" alt="" />
-            <h2>Título do Card</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magni minima nostrum inventore consequatur cumque cupiditate nam officia sapiente tempora illo repudiandae, eos blanditiis nihil! Saepe provident sed laudantium quas.</p>
-
-            <p>Tecnologias: HTML, CSS, JS</p>
-
-            <div>
-                <a href="/">Projeto (live)</a>
-                <a href="/">Repositório</a>
-            </div>
+            {children}
         </Container>
     )
 };
