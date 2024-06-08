8d7857979a1b43dcd9cd8077a33dca27b23e251b
Moiseis Cruz <moizzito@gmail.com>
2024-06-08 08:54:59 -0300

    Creating Footer function markup.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,32 @@
 export const Footer = () => {
     return(
         <footer>
+            <div>
+                <h2>Logo</h2>
 
+                <div>
+                    <p>Se desejar conversar comigo, por favor não hesite e me envie uma mensagem nas redes sociais.</p>
+
+                    <p>BeCode &#169; 2024. Alguns direitos reservados.</p>
+                </div>
+
+                <ul>
+                    <li><a href="">Projetos</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contatos</a></li>
+                </ul>
+
+                <div>
+                    <p>me siga</p>
+
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 };
