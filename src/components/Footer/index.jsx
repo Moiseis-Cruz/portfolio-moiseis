5654d98c5b1801ab17d226bfeb32b8e3f5e4ca88
Moiseis Cruz <moizzito@gmail.com>
2024-06-10 19:30:55 -0300

    Starting the footer grid styling.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,28 +7,32 @@ export const Footer = () => {
             <div>
                 <FooterLogo>MC</FooterLogo>
 
-                <div>
-                    <FooterMessage>Se desejar conversar comigo, por favor não hesite e me envie uma mensagem nas redes sociais.</FooterMessage>
+                <FooterGrid>
 
-                    <Warning>BeCode &#169; 2024. Alguns direitos reservados.</Warning>
-                </div>
+                    <div>
+                        <FooterMessage>Se desejar conversar comigo, por favor não hesite e me envie uma mensagem nas redes sociais.</FooterMessage>
 
-                <ul>
-                    <li><a href="">Projetos</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Contatos</a></li>
-                </ul>
-
-                <div>
-                    <p>me siga</p>
+                        <Warning>BeCode &#169; 2024. Alguns direitos reservados.</Warning>
+                    </div>
 
                     <ul>
-                        <li><a href="https://www.linkedin.com/in/mois%C3%A9s-cruz-b88990248/"> <FaLinkedin /> </a></li>
-                        <li><a href="https://github.com/Moiseis-Cruz"> <FaGithub /> </a></li>
-                        <li><a href="https://discord.com/channels/@me/1095845561330053131" target="_blank"> <FaDiscord /> </a></li>
-                        {/* <li><a href="">rede social</a></li> */}
+                        <li><a href="">Projetos</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Contatos</a></li>
                     </ul>
-                </div>
+
+                    <div>
+                        <p>me siga</p>
+
+                        <ul>
+                            <li><a href="https://www.linkedin.com/in/mois%C3%A9s-cruz-b88990248/"> <FaLinkedin /> </a></li>
+                            <li><a href="https://github.com/Moiseis-Cruz"> <FaGithub /> </a></li>
+                            <li><a href="https://discord.com/channels/@me/1095845561330053131" target="_blank"> <FaDiscord /> </a></li>
+                            {/* <li><a href="">rede social</a></li> */}
+                        </ul>
+                    </div>
+                </FooterGrid>
+
             </div>
         </footer>
     )
@@ -52,4 +56,15 @@ const FooterMessage = styled.p`
 
 const Warning = styled.p`
     font-size: 1.3rem;
+`
+
+const FooterGrid = styled.div`
+    background-color: gray;
+    display: grid;
+    /* grid-template-columns: repeat(3, 1fr); */
+    grid-template-columns: repeat(3, minmax(100px, 400px));
+    /* place-content: center; */
+    justify-content: space-around;
+    padding: 10px 20px;
+    gap: 5rem;
 `
