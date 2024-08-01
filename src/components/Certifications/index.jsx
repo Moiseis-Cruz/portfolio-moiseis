665003c63e440b00914bc8dadf7d1791024a3f31
Moiseis Cruz <moizzito@gmail.com>
2024-07-31 23:10:41 -0300

    I created the basis of the function.

--- src/components/Certifications/index.jsx
+++ src/components/Certifications/index.jsx
@@ -6,6 +6,25 @@ export const Certifications = () => {
 
             <p>Confira os meus principais certificados, <br/> Essenciasi para o meu desenvolvimento!</p>
 
+            <div>
+
+                <ul>
+                    <li>
+                        <div>
+                            <h3>julho, 2024</h3>
+
+                            <p>Nome do Certificado</p>
+
+                            <p>Descrição do curso concluido. Horas de aula e seu objetivo.</p>
+                        </div>
+                    </li>
+                    <li>Certificado 2</li>
+                    <li>Certificado 3</li>
+                    <li>Certificado 4</li>
+                </ul>
+
+            </div>
+
         </section>
     )
 }
