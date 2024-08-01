f72ac6510c2ed447ec38e1ab0a7acf1593c8c622
Moiseis Cruz <moizzito@gmail.com>
2024-07-31 23:10:16 -0300

    I added the Certifications component and its import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { About } from './components/About';
+import { Certifications } from './components/Certifications';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Certifications />
       <MyProjects />
       <Footer />
     </>
