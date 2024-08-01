baeb8024e628bd1a395850e18b41f78024042767
Moiseis Cruz <moizzito@gmail.com>
2024-07-31 23:01:12 -0300

    I created the Certifications file and function.

---
+++ src/components/Certifications/index.jsx
@@ -0,0 +1,11 @@
+export const Certifications = () => {
+    return(
+        <section>
+
+            <h2>Minhas Certificações</h2>
+
+            <p>Confira os meus principais certificados, <br/> Essenciasi para o meu desenvolvimento!</p>
+
+        </section>
+    )
+}
