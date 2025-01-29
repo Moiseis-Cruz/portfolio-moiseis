6ee85d68ca87f9d7131bfee0058785dad107c177
Moiseis Cruz <moizzito@gmail.com>
2025-01-28 21:01:38 -0300

    I created a mock for the certifications.

---
+++ src/components/Certifications/Certifications.mock.jsx
@@ -0,0 +1,24 @@
+import CertificadoLogicaProgramacao from "../../assets/imgs/logica-programacao-js.png"
+import CertificadoChatGPT from "../../assets/imgs/Curso-ChatGPT.png"
+import CertificationGitbub from "../../assets/imgs/Curso-Git-e-GitHub.png"
+
+export const certifications = [
+    {
+        data: "julho, 2024",
+        imagem: CertificadoLogicaProgramacao,
+        titulo: "Lógica  de Programação",
+        descricao: "Lógica de Programação: 6 horas. Ensinando como utilizar métodos, laços de repetição, condicionais, como estruturar o código, como usar funções e mais."
+    },
+    {
+        data: "julho, 2024",
+        imagem: CertificadoChatGPT,
+        titulo: "Curso ChatGPT",
+        descricao: "ChatGPT: 8 horas. Ensina a como otimizar buscas para obter melhores resultados de mais qualidade."
+    },
+    {
+        data: "julho, 2024",
+        imagem: CertificationGitbub,
+        titulo: "Curso Git & GitHub",
+        descricao: "Git & GitHub: 8 horas. Onde ensina os fundamentos de compartilhar e colaborando em projetos."
+    },
+];
