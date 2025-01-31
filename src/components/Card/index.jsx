67c8218a307d05cbf31959147563d7c79a6b57c3
Moiseis Cruz <moizzito@gmail.com>
2025-01-30 21:32:01 -0300

    I changed the added colors to variables.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -9,13 +9,13 @@ export const Card = ({children}) => {
 };
 
 const Container = styled.div`
-    background: linear-gradient(0deg, #ECB176, #6F4E37);
+    background: linear-gradient(0deg, var(--tertiary-color), var(--primary-color));
     max-width: 400px;
-    color: #F8EDE3;
+    color: var(--light-color);
     width: 100%;
     min-height: 424px;
     padding: 10px;
     border-radius: 14px 0;
     text-align: center;
-    border: 5px solid #A67B5B;
+    border: 5px solid var(--secundary-color);
 `
