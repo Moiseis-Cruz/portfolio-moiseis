d09131220a68c05a1781851d853b827313f33fc3
Moiseis Cruz <moizzito@gmail.com>
2025-02-02 10:20:34 -0300

    I removed components to add the <MainContainer /> component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { About } from './components/About';
-import { Certifications } from './components/Certifications';
 import { Footer } from './components/Footer';
+import MainContainer from './components/MainContainer';
 import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { MyProjects } from './components/MyProjects';
 
 function App() {
 
   return (
     <>
       <Header />
-      <Hero />
-      <About />
-      <Certifications />
-      <MyProjects />
+      <MainContainer />
       <Footer />
     </>
   )
