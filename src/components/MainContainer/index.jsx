6192d8157c449be51c95b67f7a0accc5513c5532
Moiseis Cruz <moizzito@gmail.com>
2025-02-02 10:23:19 -0300

    I created the <MainContainer /> component function and added its components.

---
+++ src/components/MainContainer/index.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { About } from '../../components/About';
+import { Certifications } from '../../components/Certifications';
+import { Hero } from '../../components/Hero';
+import { MyProjects } from '../../components/MyProjects';
+
+const MainContainer = () => {
+    return(
+        <Main>
+            <Hero />
+            <About />
+            <Certifications />
+            <MyProjects />
+        </Main>
+    )
+};
+
+const Main = styled.main`
+    @media (max-width: 426px) {
+        padding-inline: 10px;
+    }
+`
+
+export default MainContainer;
